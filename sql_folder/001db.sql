43d005857d57ccdb65db05711867a3a664d77715
Mohammad Hussain <msh52@njit.edu>
2023-02-15 11:35:48 -0500

    updated sql file

--- sql_folder/001db.sql
+++ sql_folder/001db.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE it490db;
-USE it490db;
-CREATE TABLE loginform (
-  id INT PRIMARY KEY,
-  user VARCHAR(20),
-  pass VARCHAR(20)
+CREATE DATABASE IT490Project;
+USE IT490Project;
+CREATE TABLE users (
+	Id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	userName varchar(128) NOT NULL,
+	userEmail varchar(128) NOT NULL,
+	userId varchar(128) NOT NULL,
+	userPass varchar(128) NOT NULL
 );
-
-
-
