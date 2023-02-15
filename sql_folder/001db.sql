281e7ca2cc8aab80973ceca461d33c051541db68
Mohammad Hussain <msh52@njit.edu>
2023-02-15 12:26:23 -0500

    updated 001db files

--- sql_folder/001db.sql
+++ sql_folder/001db.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE DATABASE IT490Project;
 USE IT490Project;
 CREATE TABLE users (
@@ -7,12 +6,4 @@ CREATE TABLE users (
 	userEmail varchar(128) NOT NULL,
 	userId varchar(128) NOT NULL,
 	userPass varchar(128) NOT NULL
-=======
-CREATE DATABASE it490db;
-USE it490db;
-CREATE TABLE loginform (
-  id INT PRIMARY KEY,
-  user VARCHAR(50) NOT NULL UNIQUE,
-  pass VARCHAR(255) NOT NULL
->>>>>>> origin/master
 );
