46400cd5a12f32de3dc4636d7cc3d3f949a4061c
Brandon Hunt <49074895+bah34@users.noreply.github.com>
2023-02-15 15:40:57 -0500

    test

--- phpproject01_LoginSystem/includes/login.inc.php
+++ phpproject01_LoginSystem/includes/login.inc.php
@@ -1,4 +1,7 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 if (isset($_POST["submit"])) {
 
@@ -21,8 +24,32 @@ if (isset($_POST["submit"])) {
   // If we get to here, it means there are no user errors
 
   // Now we insert the user into the database
-  loginUser($conn, $username, $pwd);
+  //loginUser($conn, $username, $pwd);
+  
+  
+  $client = new rabbitMQClient("../../sampleFiles/testRabbitMQ.ini","testServer");
+  if (isset($argv[1]))
+  {
+    $msg = $argv[1];
+  }
+  else
+  {
+    $msg = "test message";
+  }
 
+  $request = array();
+  $request['type'] = "Login";
+  $request['username'] = $username;
+  $request['password'] = $pwd;
+  $request['message'] = $msg;
+  $response = $client->send_request($request);
+	
+  echo "client received response: ".PHP_EOL;
+  print_r($response);
+  echo "\n\n";
+
+  echo $argv[0]." END".PHP_EOL;
+	
 } else {
 	header("location: ../login.php");
     exit();
