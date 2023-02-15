e07c2b0dbfb8189f97a542274d927abda10c87ed
Brandon Hunt <49074895+bah34@users.noreply.github.com>
2023-02-15 15:59:37 -0500

    Update login.inc.php

--- phpproject01_LoginSystem/includes/login.inc.php
+++ phpproject01_LoginSystem/includes/login.inc.php
@@ -1,7 +1,7 @@
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
+require_once('../../path.inc');
+require_once('../../get_host_info.inc');
+require_once('../../rabbitMQLib.inc');
 
 if (isset($_POST["submit"])) {
 
