2129c48f81f8254ceffac2ccbc59f62a09a8663f
brandonLaptop <bah34@njit.edu>
2023-02-22 13:31:24 -0500

    database

--- dbListener.php
+++ dbListener.php
@@ -7,8 +7,8 @@ require_once('mysqlConnect.php');
 
 function doLogin($username,$password)
 {
-	//$query = "SELECT * FROM users WHERE usersUid='$username' AND usersPwd='$password'";
-	//$result = mysqli_query(dbConnection(), $query);
+	$query = "SELECT * FROM users WHERE usersUid='$username' AND usersPwd='$password'";
+	$result = mysqli_query(dbConnection(), $query);
 	//return true;
 	//return false if not valid
 }
