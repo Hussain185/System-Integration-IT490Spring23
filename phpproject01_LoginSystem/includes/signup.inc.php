4be6e83e9d50420953d65a1ceadbe47a63e2b548
ab2397 <81304926+ab2397@users.noreply.github.com>
2023-02-27 11:07:00 -0500

    Removed uidExist()

--- phpproject01_LoginSystem/includes/signup.inc.php
+++ phpproject01_LoginSystem/includes/signup.inc.php
@@ -12,7 +12,7 @@ if (isset($_POST["submit"])) {
   // Then we run a bunch of error handlers to catch any user mistakes we can (you can add more than I did)
   // These functions can be found in functions.inc.php
 
-  require_once "dbh.inc.php";
+  // require_once "dbh.inc.php";
   require_once 'functions.inc.php';
 
   // Left inputs empty
@@ -36,16 +36,12 @@ if (isset($_POST["submit"])) {
     header("location: ../signup.php?error=passwordsdontmatch");
 		exit();
   }
-  // Is the username taken already
-  if (uidExists($conn, $username) !== false) {
-    header("location: ../signup.php?error=usernametaken");
-		exit();
-  }
+ 
 
   // If we get to here, it means there are no user errors
 
   // Now we insert the user into the database
-  createUser($conn, $name, $email, $username, $pwd);
+  // createUser($conn, $name, $email, $username, $pwd);
 
 } else {
 	header("location: ../signup.php");
