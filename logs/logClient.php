6d879603dfec0fffd5b6aab381520a1cdec94ae4
brandonLaptop <bah34@njit.edu>
2023-03-02 12:55:26 -0500

    requires

--- logs/logClient.php
+++ logs/logClient.php
@@ -1,8 +1,4 @@
 <?php
-require_once('../phpproject01_LoginSystem/incFiles/path.inc');
-require_once('../phpproject01_LoginSystem/incFiles/get_host_info.inc');
-require_once('../phpproject01_LoginSystem/incFiles/rabbitMQLib.inc');
-
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ini_set('log_errors', 'On');
