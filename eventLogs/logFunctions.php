34ebc8d84c8b4a751b436441fe9ec0fc7ba9b610
bhunt222 <bhunt2018@gmail.com>
2023-03-07 20:58:14 -0500

    log setup

--- eventLogs/logFunctions.php
+++ eventLogs/logFunctions.php
@@ -1,5 +1,7 @@
 <?php
-
+require_once('../sampleFiles/path.inc');
+require_once('../sampleFiles/get_host_info.inc');
+require_once('../sampleFiles/rabbitMQLib.inc');
 
 
 function logClient($type, $log)
--- eventLogs/logListener.php
+++ eventLogs/logListener.php
@@ -1,6 +1,7 @@
 <?php
-require_once('logFunctions.php');
-
+require_once('../sampleFiles/path.inc');
+require_once('../sampleFiles/get_host_info.inc');
+require_once('../sampleFiles/rabbitMQLib.inc');
 
 
 function generateLog($logMsg) {
