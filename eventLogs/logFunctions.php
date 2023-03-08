1240c71a6ae4fac53a237443333137114534e96f
brandonLaptop <bah34@njit.edu>
2023-03-08 11:51:41 -0500

    log stuff

--- eventLogs/logFunctions.php
+++ eventLogs/logFunctions.php
@@ -3,13 +3,13 @@ require_once('../sampleFiles/path.inc');
 require_once('../sampleFiles/get_host_info.inc');
 require_once('../sampleFiles/rabbitMQLib.inc');
 
-
-function logClient($type, $log)
+function logClient($type, $machine, $log)
 {
     $client = new rabbitMQClient("log.ini","logServer");
 
     $request = array();
     $request['type'] = $type;
+    $request['machine'] = $machine;
     $request['log'] = $log;
     $response = $client->send_request($request);
 
--- eventLogs/logListener.php
+++ eventLogs/logListener.php
@@ -3,34 +3,21 @@ require_once('../sampleFiles/path.inc');
 require_once('../sampleFiles/get_host_info.inc');
 require_once('../sampleFiles/rabbitMQLib.inc');
 
-
-function generateLog($logMsg) {
-    //write to log file
-
-
-}
-
 function logHandler($request) {
     var_dump($request);
-    if(!isset($request['type'])){
-        return "ERROR: unsupported message type";
+
+    $logFile = fopen("logs.txt", "a");
+
+    if(!isset($request['machine'])){
+        return "ERROR: unsupported log message type";
         //log this error
+        fwrite($logFile, "invalid log message type\n");
     }
-    switch ($request['type'])
-    {
-        case "db":
-            //db
-            generateLog($request['log']);
-            break;
-        case "frontend":
-            //frontend
-            generateLog($request['log']);
-            break;
-        case "dmz":
-            //dmz
-            generateLog($request['log']);
-            break;
-    }
+
+
+    fwrite($logFile, $request['type']." ".$request['machine']." ".$request['log']." ".time()."\n");
+    fclose($logFile);
+
     return array("returnCode" => '0', 'message'=>"log server received request and processed");
 }
 
