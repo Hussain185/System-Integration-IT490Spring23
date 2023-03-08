2cfe2573f2638fcbe523709d3b47337562b7cc7c
brandonLaptop <bah34@njit.edu>
2023-03-08 12:33:55 -0500

    idk

--- eventLogs/logFunctions.php
+++ eventLogs/logFunctions.php
@@ -1,8 +1,4 @@
 <?php
-require_once('../sampleFiles/path.inc');
-require_once('../sampleFiles/get_host_info.inc');
-require_once('../sampleFiles/rabbitMQLib.inc');
-
 function logClient($type, $machine, $log)
 {
     $client = new rabbitMQClient("log.ini","logServer");
