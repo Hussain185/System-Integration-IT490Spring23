a7e6754264f89bb99d0a989810635f96b6ae0999
ab2397 <81304926+ab2397@users.noreply.github.com>
2023-03-09 12:36:28 -0500

    Update script.js

--- phpproject01_LoginSystem/js/script.js
+++ phpproject01_LoginSystem/js/script.js
@@ -57,7 +57,7 @@ function SendLoginRequest(username,password)
 	request.onreadystatechange= function ()
 	{
 		
-		if ((this.readyState == 4)&&(this.status == 200))
+		if (this.status == 200)
 		{
 			alert(httpRequest.responseText);
 			HandleLoginResponse(this.responseText);
