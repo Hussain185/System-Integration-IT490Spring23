9444879748f7b56b4d48cc48ffd9523f4efd848c
ab2397 <81304926+ab2397@users.noreply.github.com>
2023-03-14 14:01:59 -0400

    Edited createUser, Remove outdated functions

--- dbFunctions.php
+++ dbFunctions.php
@@ -12,7 +12,7 @@ function doLogin($username,$password)
     if($result){
 		if($result->num_rows == 0){
 			echo("No users in table.");
-            //logClient('DB Error','database','No users in table');
+            logClient('DB Error','database','No users in table');
 			// $event = date("Y-m-d") . "  " . date("h:i:sa") . " [ DB ] " . "ERROR: this user does not exist: $username" . "\n";
 	                // log_event($event);
 			$myNum= 0;
@@ -62,8 +62,10 @@ function uidExists($conn, $username) {
     $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../signup.php?error=stmtfailed");
-        exit();
+        // header("location: ../signup.php?error=stmtfailed");
+        // exit();
+        $result = false;
+        return $result;
     }
 
     mysqli_stmt_bind_param($stmt, "ss", $username, $username);
@@ -84,13 +86,17 @@ function uidExists($conn, $username) {
 }
 
 // Insert new user into database
+// If $conn does not work, then replace with dbConnection()
 function createUser($conn, $name, $email, $username, $pwd) {
     $sql = "INSERT INTO users (usersName, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?);";
 
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../signup.php?error=stmtfailed");
-        exit();
+        // header("location: ../signup.php?error=stmtfailed");
+        // exit();
+		$myNum= 0;
+		$myJSON = json_encode($myNum);
+		return $myJSON;
     }
 
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
@@ -99,55 +105,11 @@ function createUser($conn, $name, $email, $username, $pwd) {
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
-    header("location: ../signup.php?error=none");
-    exit();
-}
-
-function emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) {
-    $result = null;
-    if (empty($name) || empty($email) || empty($username) || empty($pwd) || empty($pwdRepeat)) {
-        $result = true;
-    }
-    else {
-        $result = false;
-    }
-    return $result;
-}
-
-// Check invalid username
-function invalidUid($username) {
-    $result = null;
-    if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        $result = true;
-    }
-    else {
-        $result = false;
-    }
-    return $result;
-}
-
-// Check invalid email
-function invalidEmail($email) {
-    $result = null;
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $result = true;
-    }
-    else {
-        $result = false;
-    }
-    return $result;
-}
-
-// Check if passwords matches
-function pwdMatch($pwd, $pwdrepeat) {
-    $result = null;
-    if ($pwd !== $pwdrepeat) {
-        $result = true;
-    }
-    else {
-        $result = false;
-    }
-    return $result;
+	$myNum= 1;
+	$myJSON = json_encode($myNum);
+	return $myJSON;
+    // header("location: ../signup.php?error=none");
+    // exit();
 }
 
 // Log user into website
@@ -155,18 +117,22 @@ function loginUser($conn, $username, $pwd) {
     $uidExists = uidExists($conn, $username);
 
     if ($uidExists === false) {
-        header("location: ../login.php?error=wronglogin");
-        exit();
-    }
+		$myNum= 0;
+		$myJSON = json_encode($myNum);
+		return $myJSON;
+	}
 
     $pwdHashed = $uidExists["usersPwd"];
     $checkPwd = password_verify($pwd, $pwdHashed);
 
     if ($checkPwd === false) {
-        header("location: ../login.php?error=wronglogin");
-        exit();
-    }
+		$myNum= 0;
+		$myJSON = json_encode($myNum);
+		return $myJSON; 
+	}
+	
     elseif ($checkPwd === true) {
+		// Replace code below with doLogin
         session_start();
         $_SESSION["userid"] = $uidExists["usersId"];
         $_SESSION["useruid"] = $uidExists["usersUid"];
