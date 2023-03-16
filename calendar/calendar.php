f0231e1612d52b6a54e306463f386eb9b786774d
brandonLaptop <bah34@njit.edu>
2023-03-16 15:33:14 -0400

    calendar stuff

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -33,6 +33,33 @@ include 'calendarFunctions.php';
                     <div>sat</div>
                 </div>
             </div>
+            <div class="newEventButton">
+                <button class="create-event-button" onclick="newEventForm()">Create Event</button>
+
+                <div class="event-form id="myEvent">
+                    <form action="/action_page.php" class="form-container">
+                        <h1>Login</h1>
+
+                        <label for="eventTitle"><b>Event Title</b></label>
+                        <input type="text" placeholder="Enter Event Name" name="event" required>
+
+                        <label for="description"><b>Description</b></label>
+                        <input type="text" placeholder="Enter Description" name="desc" required>
+
+                        <label for="startDate"><b>Start Date</b></label>
+                        <input type="datetime-local" name="eventDate" required>
+
+                        <label for="eventLength"><b>Length of Event</b></label>
+                        <input type="number" name="days">
+
+                        <label for="color"><b>Event Color</b></label>
+                        <input type="color">
+
+                        <button type="submit" class="btn">Login</button>
+                        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                    </form>
+                </div>
+            </div>
 		</div>
 	</body>
 </html>
