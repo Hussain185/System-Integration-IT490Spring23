0bff54d4af8fc833913a104275d8d27227cfc36b
vic292 <123338639+vic292@users.noreply.github.com>
2023-03-18 16:37:23 -0400

    Added basic filter functions

--- recipeApp/recipeSearchFilter/scriptRecipeSearchFilter.js
+++ recipeApp/recipeSearchFilter/scriptRecipeSearchFilter.js
@@ -1,11 +1,7 @@
-const form = document.querySelector('form');
-const searchResult = document.querySelector('.search');
-const container = document.querySelector('.conatiner');
-let userQuery = '';
-
+//Welcome message
 var i = 0;
-var txt = "Hello";
-var speed = 50;
+var txt = "Welcome";
+var speed = 100;
 
 function typeWriter() {
     if(i<txt.length){
@@ -14,19 +10,76 @@ function typeWriter() {
         setTimeout(typeWriter, speed);
     }
 }
+//
+
+// //Filter dropdown
+// var expanded = false;
+// function showCheckboxes() {
+//     var checkboxes = document.getElementById("checkboxes");
+//     if(!expanded) {
+//         checkboxes.style.display = "block";
+//         expanded = true;
+//     }
+//     else {
+//         checkboxes.style.display = "none";
+//         expanded = false;
+//     }
+// }
+// //
+
+
+//API search/URL builder
+
+const form = document.querySelector('form');
+const searchResult = document.querySelector('.search');
+const container = document.querySelector('.container');
+let userQuery = '';
+
+let dietLabels = [];
+let cuisineType = [];
+let mealType = [];
+// let minCal = '';
+// let maxCal = '';
 
 const ID = "b14f1b2d";
 const key = "b7d53cad8c74e29b857054d820b2ab4c";
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+
     userQuery = e.target.querySelector('input').value;
     console.log(userQuery);
+
+    dietLabels = e.target.querySelector('select[name="dietLabels"]').value;
+    console.log(dietLabels);
+
+    cuisineType = e.target.querySelector('select[name="cuisineType"]').value;
+    console.log(cuisineType);
+
+    mealType = e.target.querySelector('select[name="mealType"]').value;
+    console.log(mealType);
+
+    // minCal = document.querySelector('input[name="minCalories"]');
+    // maxCal = document.querySelector('input[name="maxCalories"]');
+
     fetchData();
 })
 
 async function fetchData(){
-    const baseURL = `https://api.edamam.com/search?q=${userQuery}&app_id=${ID}&app_key=${key}`;
+    let baseURL = `https://api.edamam.com/search?q=${userQuery}&app_id=${ID}&app_key=${key}`; //&diet=${dietLabels}&cuisineType=${cuisineType}&mealType=${mealType}&calories=${minCal}-${maxCal}`;   //&diet=${dietLabels}
+    
+    if (dietLabels.length) {
+        baseURL += "&diet=" + dietLabels;
+    }
+
+    if (mealType.length) {
+        baseURL += "&mealType=" + mealType;
+    }
+
+    if (cuisineType.length) {
+        baseURL += "&cuisineType=" + cuisineType;
+    }
+
     const response = await fetch(baseURL);
     const data = await response.json();
     createContent(data.hits);
