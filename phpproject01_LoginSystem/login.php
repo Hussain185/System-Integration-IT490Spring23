32c7f1131bc1dfa154d511b8e268edbafdabf933
ab2397 <81304926+ab2397@users.noreply.github.com>
2023-03-19 11:47:57 -0400

    Revamp entire website (Hope it works...)

--- phpproject01_LoginSystem/login.php
+++ phpproject01_LoginSystem/login.php
@@ -1,32 +1,54 @@
-<?php
-  include_once 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<section class="signup-form">
-  <h2>Log In</h2>
-  <div class="signup-form-form">
-      <input type="text" id="ajaxTextUser" placeholder="Username/Email">
-      <input type="password" id="ajaxTextPwd" placeholder="Password">
-      <button type="submit" id="ajaxButton">Log in</button>
+    <head>
+        <title>Register</title>
+        <?php
+			include_once 'header.php';
+		?>
+		
+  <div class="auth-content">
+
+    <!-- <form action="register.html" method="post"> -->
+	<section class="signup-form">
+      <h2 class="form-title">Register</h2>
+
+      <!-- <div class="msg error">
+        <li>Username required</li>
+      </div> -->
+
+	  <div>
+        <label>Full Name</label>
+        <input type="text" id="ajaxTextName" class="text-input">
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" id="ajaxTextEmail" class="text-input">
+      </div>
+	  <div>
+        <label>Username</label>
+        <input type="text" id="ajaxTextUser" class="text-input">
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" id="ajaxTextPwd" class="text-input">
+      </div>
+      <div>
+        <label>Password Confirmation</label>
+        <input type="password" id="ajaxTextRptPwd" class="text-input">
+      </div>
+      <div>
+        <button type="submit" id="ajaxButton" class="btn btn-big">Register</button>
+      </div>
+      <p>Or <a href="login.php">Sign In</a></p>
+    </section>
+
   </div>
-  <?php
-  /*
-    // Error messages
-    if (isset($_GET["error"])) {
-      if ($_GET["error"] == "emptyinput") {
-        echo "<p>Fill in all fields!</p>";
-      }
-      else if ($_GET["error"] == "wronglogin") {
-        echo "<p>Wrong login!</p>";
-      }
-    }
-    */
-  ?>
-</section>
-
-<?php
-  include_once 'footer.php';
-?>
+  
+  <!-- // Page Wrapper -->
+ 	<?php 
+		include_once 'footer.php';
+	?>
 <script>
   document.getElementById("ajaxButton").addEventListener('click', function() {
     const userName = document.getElementById("ajaxTextUser").value;
