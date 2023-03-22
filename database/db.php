601d878f5bad606174155386491ec70b07a6c7e0
Hussain185 <msh52@njit.edu>
2023-03-22 12:01:05 -0400

    new edit

--- database/db.php
+++ database/db.php
@@ -8,13 +8,93 @@ function dd($value) //only to see the variable printed on display
 
 }
 
-function selectAll($table) //shows all the data in a table 
+function executeQuery($sql, $data)
 {
     global $conn;
+    $stmt = $conn->prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s', count($values));
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    return $stmt;
+}
+
+
+
+function selectAll($table) //shows all the data in a table 
+{
+    global $conn;  
     $sql = "SELECT * FROM $table";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     return $records;
     
+}
+
+
+function selectOne($table, $conditions)
+{
+    global $conn;
+    $sql = "SELECT * FROM $table";
+
+    $i = 0;
+    foreach ($conditions as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " WHERE $key=?";
+        } else {
+            $sql = $sql . " AND $key=?";
+        }
+        $i++;
+    }
+
+    $sql = $sql . " LIMIT 1";
+    $stmt = executeQuery($sql, $conditions);
+    $records = $stmt->get_result()->fetch_assoc();
+    return $records;
+}
+
+//sql = "INSERT INTO users SET userName= ?, userEmail = ?, usersUid = ?, usersPwd = ?"
+function create($table, $data)
+{
+    global $conn;
+    $sql = "INSERT INTO $table SET ";
+
+    $i = 0;
+    foreach ($data as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " $key=?";
+        } else {
+            $sql = $sql . ", $key=?";
+        }
+        $i++;
+    }
+    
+    $stmt = executeQuery($sql, $data);
+    $id = $stmt->insert_id;
+    return $id;
+}
+
+
+
+//sql = "UPDATE users SET userName= ?, userEmail = ?, usersUid = ?, usersPwd = ? WHERE id=?"
+function update($table, $id, $data)
+{
+    global $conn;
+    $sql = "UPDATE $table SET ";
+
+    $i = 0;
+    foreach ($data as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " $key=?";
+        } else {
+            $sql = $sql . ", $key=?";
+        }
+        $i++;
+    }
+
+    $sql = $sql . " WHERE id=?";
+    $data['id'] = $id;
+    $stmt = executeQuery($sql, $data);
+    return $stmt->affected_rows;
 }
