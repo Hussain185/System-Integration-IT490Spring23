99984eba7a370e5380a2a4b64ca002034d0fdc2a
brandonLaptop <bah34@njit.edu>
2023-03-22 12:12:58 -0400

    log stuff

--- phpproject01_LoginSystem/includes/signup.inc.php
+++ phpproject01_LoginSystem/includes/signup.inc.php
@@ -4,6 +4,7 @@ require_once('../../sampleFiles/get_host_info.inc');
 require_once('../../sampleFiles/rabbitMQLib.inc');
 //require_once("dbh.inc.php");
 require_once('functions.inc.php');
+require_once('../../eventLogs/logFunctions.php');
 
   // First we get the form data from the URL
   $name = $_POST["name"];
@@ -22,21 +23,25 @@ require_once('functions.inc.php');
   // We set the functions "!== false" since "=== true" has a risk of giving us the wrong outcome
   if (emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) !== false) {
     header("location: ../signup.php?error=emptyinput");
+    logClient('User Input', 'frontend', 'Signup form empty');
 		exit();
   }
 	// Proper username chosen
   if (invalidUid($username) !== false) {
     header("location: ../signup.php?error=invaliduid");
+    logClient('User Input','frontend','User UID invalid');
 		exit();
   }
   // Proper email chosen
   if (invalidEmail($email) !== false) {
     header("location: ../signup.php?error=invalidemail");
+    logClient('User Input','frontend','Invalid Email');
 		exit();
   }
   // Do the two passwords match?
   if (pwdMatch($pwd, $pwdRepeat) !== false) {
     header("location: ../signup.php?error=passwordsdontmatch");
+    logClient('User Input','frontend','Passwords do not match');
 		exit();
   }
 
