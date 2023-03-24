47c2bebed8411ab6ef87e2df786fb447a86ff220
Hussain185 <msh52@njit.edu>
2023-03-24 08:06:06 -0400

    new edit to create.php

--- phpproject01_LoginSystem/create.php
+++ phpproject01_LoginSystem/create.php
@@ -1,18 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Add Food Post</title>
-    <?php include_once 'header.php'; ?>
-</head>
-<body>
-    <!-- Admin Page Wrapper -->
-    <div class="admin-wrapper">
-        <!-- Admin Content -->
-        <div class="admin-content">
-            <div class="content">
-                <h2 class="page-title">Manage Posts</h2>
-                <section class="blog-form">
-                    <form id="add-post-form" method="POST">
+
+    <head>
+        <title>Add Food Post</title>
+        <?php
+			include_once 'header.php';
+		?>
+		
+        <!-- Admin Page Wrapper -->
+        <div class="admin-wrapper">
+
+            <!-- Left Sidebar 
+            <div class="left-sidebar">
+                <ul>
+                    <li><a href="index.html">Manage Posts</a></li>
+                    <li><a href="../users/index.html">Manage Users</a></li>
+                    <li><a href="../topics/index.html">Manage Topics</a></li>
+                </ul>
+            </div>
+            -->
+
+
+            <!-- Admin Content -->
+            <div class="admin-content">
+		<!--
+                <div class="button-group">
+                    <a href="create.html" class="btn btn-big">Add Post</a>
+                    <a href="index.html" class="btn btn-big">Manage Posts</a>
+                </div>
+		-->
+
+                <div class="content">
+
+                    <h2 class="page-title">Manage Posts</h2>
+
+                    <section class="blog-form">
                         <div>
                             <label>Title</label>
                             <input type="text" name="title" id="AjaxTitle" class="text-input">
@@ -21,37 +43,44 @@
                             <label>Body</label>
                             <textarea name="body" id="body"></textarea>
                         </div>
-                    <!--
+			<!--
                         <div>
-                        <label>Image</label>
-                        <input type="file" name="image" class="text-input">
-                    </div>
-                    <div>
-                        <label>Topic</label>
-                        <select name="topic" class="text-input">
-                            <option value="Poetry">Poetry</option>
-                            <option value="Life Lessons">Life Lessons</option>
-                        </select>
-                    </div>
-                    -->
-                    <div>
+                            <label>Image</label>
+                            <input type="file" name="image" class="text-input">
+                        </div>
+                        <div>
+                            <label>Topic</label>
+                            <select name="topic" class="text-input">
+                                <option value="Poetry">Poetry</option>
+                                <option value="Life Lessons">Life Lessons</option>
+                            </select>
+                        </div>
+			-->
+                        <div>
                             <button type="submit" id="ajaxButton" class="btn btn-big">Add Post</button>
                         </div>
                     </form>
-                </section>
+
+                </div>
+
             </div>
+            <!-- // Admin Content -->
+
         </div>
-        <!-- // Admin Content -->
-    </div>
-    <!-- // Page Wrapper -->
-    <?php include_once 'footer.php'; ?>
-    <script>
-        document.getElementById("ajaxButton").addEventListener('click', function() {
+        <!-- // Page Wrapper -->
+		
+		<?php 
+			include_once 'footer.php';
+		?>
+		<script>
+			document.getElementById("ajaxButton").addEventListener('click', function() {
             const title = document.getElementById("AjaxTitle").value;
             const bodytext = document.getElementById("body").value;
            // const topic = document.getElementsByName("topic")[0].value;
             //const image = document.getElementsByName("image")[0].files[0];
-            SendPostRequest(title, bodytext,topic,image);
+           // SendPostRequest(title, bodytext,topic,image);
+            SendPostRequest(title, bodytext);
+
         });
 
        // function SendPostRequest(title, body, topic, image) {
@@ -65,6 +94,6 @@
             xhr.open('POST', 'add_post.php', true);
             xhr.send(formData);
         }
-    </script>
-</body>
+		</script>
+
 </html>
