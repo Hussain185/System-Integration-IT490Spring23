9e7c10330489fd9ecc3f8221b1e8fbffc2d91016
bhunt222 <bhunt2018@gmail.com>
2023-03-24 16:17:29 -0400

    db functions

--- database/dbFunctions.php
+++ database/dbFunctions.php
@@ -218,12 +218,7 @@ function searchDB($conn, $query, $dietLabels, $cuisineType, $mealType)
             return $myJSON;
         }
 
-        mysqli_stmt_bind_param($stmt, "ssssssss", $response[0],$response[1],$response[2],$response[3],
-            $query, $dietLabels, $cuisineType, $mealType);
-        mysqli_stmt_execute($stmt);
-
-
-        for($i = 0;$i < count($response);$i+4){
+        for($i = 0;$i < count($response);$i+=4){
             mysqli_stmt_bind_param($stmt, "ssssssss", $response[$i],$response[$i+1],$response[$i+2],$response[$i+3],
             $query, $dietLabels, $cuisineType, $mealType);
             mysqli_stmt_execute($stmt);
