b5969dbcc6e4ca18e9d2c89a22f09c48c17d3fea
bhunt222 <bhunt2018@gmail.com>
2023-04-18 21:09:08 -0400

    create_mode

--- deployment/testssh.php
+++ deployment/testssh.php
@@ -8,12 +8,12 @@ $command = "ls -la";
 $session = ssh2_connect($sshServer,22);
 ssh2_auth_password($session, $user, $password);
 
-ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', true);
+ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', 0644);
 
 //$stream = ssh2_exec($session,$command);
 //stream_set_blocking($stream, true);
 //$output = stream_get_contents($stream);
 
-echo $output;
+//echo $output;
 
 echo 'done';
