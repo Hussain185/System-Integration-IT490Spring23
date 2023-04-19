1ff23ca81239d31333390a1ee5542010c49f7719
bhunt222 <bhunt2018@gmail.com>
2023-04-18 20:43:04 -0400

    fixes

--- deployment/testssh.php
+++ deployment/testssh.php
@@ -6,7 +6,7 @@ $command = "ls -la";
 
 $conn = ssh2_connect($sshServer,22);
 ssh2_auth_password($conn, $user, $password);
-$stream = ssh2_exec($sshServer.$command);
+$stream = ssh2_exec($conn,$command);
 stream_set_blocking($stream, true);
 $output = stream_get_contents($stream);
 
