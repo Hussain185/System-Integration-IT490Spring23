a89e7db87c3c3b198b873780d40c6a329cab3fc8
bhunt222 <bhunt2018@gmail.com>
2023-04-19 13:47:44 -0400

    script

--- deployment/testssh.php
+++ deployment/testssh.php
@@ -8,40 +8,39 @@ $command = "ls -la";
 $session = ssh2_connect($sshServer,22);
 ssh2_auth_password($session, $user, $password);
 
-//ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', 0644);
+//scp for file transfer should be recursive with the -r flag
+$command = 'scp -r /path/to/source brandon@10.147.18.0:/path/to/destination';
+$stream = ssh2_exec($session, $command);
+stream_set_blocking($stream, true);
+$output = stream_get_contents($stream);
+echo $output;
 
-//$stream = ssh2_exec($session,$command);
-//stream_set_blocking($stream, true);
-//$output = stream_get_contents($stream);
-
-//echo $output;
-
-$srcFile = '/home/brandon/git/IT490Spring23/deployment/test.txt';
-$dstFile = '/home/brandon/changes/test/test.txt';
-
-$sftp = ssh2_sftp($session);
-
-$sftpStream = @fopen('ssh2.sftp://'.$sftp.$dstFile, 'w');
-
-try {
-
-    if (!$sftpStream) {
-        throw new Exception("Could not open remote file: $dstFile");
-    }
-
-    $data_to_send = @file_get_contents($srcFile);
-
-    if ($data_to_send === false) {
-        throw new Exception("Could not open local file: $srcFile.");
-    }
-
-    if (@fwrite($sftpStream, $data_to_send) === false) {
-        throw new Exception("Could not send data from file: $srcFile.");
-    }
-
-    fclose($sftpStream);
-
-} catch (Exception $e) {
-    error_log('Exception: ' . $e->getMessage());
-    fclose($sftpStream);
-}
+//$srcFile = '/home/brandon/git/IT490Spring23/deployment/test.txt';
+//$dstFile = '/home/brandon/changes/test/test.txt';
+//
+//$sftp = ssh2_sftp($session);
+//
+//$sftpStream = @fopen('ssh2.sftp://'.$sftp.$dstFile, 'w');
+//
+//try {
+//
+//    if (!$sftpStream) {
+//        throw new Exception("Could not open remote file: $dstFile");
+//    }
+//
+//    $data_to_send = @file_get_contents($srcFile);
+//
+//    if ($data_to_send === false) {
+//        throw new Exception("Could not open local file: $srcFile.");
+//    }
+//
+//    if (@fwrite($sftpStream, $data_to_send) === false) {
+//        throw new Exception("Could not send data from file: $srcFile.");
+//    }
+//
+//    fclose($sftpStream);
+//
+//} catch (Exception $e) {
+//    error_log('Exception: ' . $e->getMessage());
+//    fclose($sftpStream);
+//}
