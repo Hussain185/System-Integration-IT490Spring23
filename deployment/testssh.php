f4084fea46e0271b4f4b3cdedc33768ca3772702
bhunt222 <bhunt2018@gmail.com>
2023-04-18 21:21:23 -0400

    ssh test

--- deployment/testssh.php
+++ deployment/testssh.php
@@ -8,7 +8,7 @@ $command = "ls -la";
 $session = ssh2_connect($sshServer,22);
 ssh2_auth_password($session, $user, $password);
 
-ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', 0644);
+//ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', 0644);
 
 //$stream = ssh2_exec($session,$command);
 //stream_set_blocking($stream, true);
@@ -16,4 +16,32 @@ ssh2_scp_send($session, '../test.txt','~/changes/test/test.txt', 0644);
 
 //echo $output;
 
-echo 'done';
+$srcFile = '/home/brandon/git/IT490Spring23/deployment/test.txt';
+$dstFile = '/home/brandon/changes/test/test.txt';
+
+$sftp = ssh2_sftp($session);
+
+$sftpStream = @fopen('ssh2.sftp://'.$sftp.$dstFile, 'w');
+
+try {
+
+    if (!$sftpStream) {
+        throw new Exception("Could not open remote file: $dstFile");
+    }
+
+    $data_to_send = @file_get_contents($srcFile);
+
+    if ($data_to_send === false) {
+        throw new Exception("Could not open local file: $srcFile.");
+    }
+
+    if (@fwrite($sftpStream, $data_to_send) === false) {
+        throw new Exception("Could not send data from file: $srcFile.");
+    }
+
+    fclose($sftpStream);
+
+} catch (Exception $e) {
+    error_log('Exception: ' . $e->getMessage());
+    fclose($sftpStream);
+}
