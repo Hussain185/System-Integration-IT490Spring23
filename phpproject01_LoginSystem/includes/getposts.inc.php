a35f56896e44a2ceb37949bfa0583ef1a45b4ae3
ab2397 <81304926+ab2397@users.noreply.github.com>
2023-04-24 12:28:11 -0400

    Add RMQ Code

--- phpproject01_LoginSystem/includes/getposts.inc.php
+++ phpproject01_LoginSystem/includes/getposts.inc.php
@@ -1,12 +1,19 @@
 <?php
-require_once "dbh.inc.php";
-require_once "functions.inc.php";
-
-// Establish database connection
-$conn = dbConnection();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once('../../sampleFiles/path.inc');
+require_once('../../sampleFiles/get_host_info.inc');
+require_once('../../sampleFiles/rabbitMQLib.inc');
+//require_once("dbh.inc.php");
+require_once('functions.inc.php');
 
 // Call the getPosts function defined in functions.php
-$posts = getAllPosts($conn);
+$client = new rabbitMQClient("../../ini/db.ini","dbServer");
+$msg = $argv[1] ?? "test message";
+$request = array();
+$request['type'] = 'add_post';
+
+$response = $client->send_request($request);
 
 // Encode the results as JSON and send them back to the client
 echo json_encode($posts);
