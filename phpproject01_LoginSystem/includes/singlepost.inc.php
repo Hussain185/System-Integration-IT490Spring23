2acf6d49b95392f15d77d30749a640ac65189eff
Hussain185 <msh52@njit.edu>
2023-04-24 11:44:40 -0400

    add posts

--- phpproject01_LoginSystem/includes/singlepost.inc.php
+++ phpproject01_LoginSystem/includes/singlepost.inc.php
@@ -6,7 +6,8 @@ require_once "functions.inc.php";
 $conn = dbConnection();
 
 // Call the getPosts function defined in functions.php
-$posts = getAllPosts($conn);
+$post = getPost($conn, $_GET['post_id']);
 
 // Encode the results as JSON and send them back to the client
-echo json_encode($posts);
+echo json_encode($post);
+
