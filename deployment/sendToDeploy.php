931e877a20af071f5a9e72fb4a0e5a911326378f
bhunt222 <bhunt2018@gmail.com>
2023-04-25 11:10:12 -0400

    comment out zip

--- deployment/sendToDeploy.php
+++ deployment/sendToDeploy.php
@@ -68,7 +68,7 @@ $version = rtrim(fgets(STDIN));
 echo "\nWhat is the file path for your changes? ";
 $file_path = rtrim(fgets(STDIN));
 
-Zip($file_path, "/zips/".$feature.$version.".tar");
+//Zip($file_path, "/zips/".$feature.$version.".tar");
 
 $sshConnection = ssh2_connect('10.147.18.0', 22);
 ssh2_auth_password($sshConnection, 'brandon', 'password');
