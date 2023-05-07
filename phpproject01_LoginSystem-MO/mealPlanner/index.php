53a10ddc991761baf3cb35f19eb28760e619a59a
vic292 <123338639+vic292@users.noreply.github.com>
2023-05-07 17:57:34 -0400

    Changed placeholder and alert message

--- phpproject01_LoginSystem-MO/mealPlanner/index.php
+++ phpproject01_LoginSystem-MO/mealPlanner/index.php
@@ -1,151 +1,186 @@
-<?php require '../includes/dbh.inc.php'; ?>
 <a href="../index.php">HOME</a>
-<!DOCTYPE html>
-<!-- Used template by CodexWorld, and library "fullcalendar" -->
-<html lang="en">
+<?php
+    //session_start();
+    require '../includes/dbh.inc.php';
+    include('tasks.php');
+    $obj = new Tasks;
+    
+    $usersUid = $_COOKIE['username'];
 
+    if(isset($_POST['submit'])) {
+        // Insert Data in the Table
+        $task = $_POST['task'];
+        $id = $_POST['id'];
+        $created_at = $updated_at = date("Y-m-d H:i:s");
+
+        //Update
+        if(!empty($id)) {
+            $sql = "UPDATE todolists set task = '".$task."', updated_at = '".$updated_at."' where id = ".$id;
+            $res = $obj->executeQuery($sql);
+            if($res) {
+                $_SESSION['success'] = "Ingredient has been updated successfully";
+            }
+            else {
+                $_SESSION['error'] = "Something went wrong";
+            }
+        }   
+        else {
+            $sql = "INSERT INTO todolists (task, created_at, updated_at, usersUid) VALUES ('".$task."', '".$created_at."', '".$updated_at."', '".$usersUid."')";
+            //$sql = "UPDATE `schedule_list` set `title` = '{$title}', `description` = '{$description}', `start_datetime` = '{$start_datetime}', `end_datetime` = '{$end_datetime}', `usersUid` =  '{$usersUid}' where `id` = '{$id}'";
+            $res = $obj->executeQuery($sql);
+
+            if($res) {
+                $_SESSION['success'] = "Ingredient has been created successfully";
+            }
+            else {
+                $_SESSION['error'] = "Something went wrong";
+            }
+        }
+        
+        session_write_close();
+        header("LOCATION:index.php");
+    }
+
+    //Get all Tasks
+    $tasks = $obj->getAllTasks();
+
+    //Get Task
+    $editing = false;
+    if(isset($_GET['action']) && $_GET['action']  === 'edit') {
+        $taskData = $obj->getTask($_GET['id']);
+        $editing = true;
+    }
+
+    //Delete Task
+    if(isset($_GET['action']) && $_GET['action']  === 'delete') {
+        $sql = "DELETE FROM todolists WHERE id = ".$_GET['id'];
+        $res = $obj->executeQuery($sql);
+        if($res) {
+            $_SESSION['success'] = "Task has been deleted successfully";
+        }
+        else {
+            $_SESSION['error'] = "Something went wrong, please try again later";
+        }
+
+        session_write_close();
+        header("LOCATION:index.php");
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meal Planner</title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./fullcalendar/lib/main.min.css">
-    <script src="./js/jquery-3.6.0.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./fullcalendar/lib/main.min.js"></script>
-    <style>
-        :root {
-            --bs-success-rgb: 71, 222, 152 !important;
-        }
-
-        html,
-        body {
-            height: 100%;
-            width: 100%;
-            /* font-family: Apple Chancery, cursive; */
-        }
-
-        .btn-info.text-light:hover,
-        .btn-info.text-light:focus {
-            background: #000;
-        }
-        table, tbody, td, tfoot, th, thead, tr {
-            border-color: #ededed !important;
-            border-style: solid;
-            border-width: 1px !important;
-        }
-    </style>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../../css/assets/fontawesome/css/all.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css"/>
+    <title>Ingredient Search and Shopping List</title>
 </head>
 
-<body class="bg-light">
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient" id="topNavBar">
+<!-- Font Awesome -->
+<link rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+            crossorigin="anonymous">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+rel="stylesheet">
+
+<!-- Custom Styling -->
+<link rel="stylesheet" href="../css/style.css">
+
+<body>
+    <div class="split left"></div>
+      <h1>Ingredient Search</h1>
+      <!-- <form onsubmit="search(event)"> -->
+      <form></form>
+        <input type="text" id="search-box" placeholder="Search for items" />
+        <!-- <button type="submit">Search</button> -->
+        <button type="submit" onclick="shoprite()">ShopRite</button>
+        <button type="submit" onclick="traderjoes()">Trader Joe's</button>
+        <button type="submit" onclick="walmart()">Walmart</button>
+        <button type="submit" onclick="wholefoods()">Whole Foods</button>
+      </form>
+      <!-- <iframe id="search-results-fr" name="iframefr" width="500" height="300"></iframe>
+      <iframe id="search-results-en" name="iframeen" width="500" height="300"></iframe> -->
+    </div>
+
+
+
+    <div class="split right">
+        <!--Step 1: Basic structure of Todo List-->
         <div class="container">
-            <a class="navbar-brand" href="https://sourcecodester.com">
-            Sourcecodester
-            </a>
+        <!--Step 2: Create input place and button-->
+        <div id="newtask">
+            <?php include('alert.php') ?>
 
-            <div>
-                <b class="text-light">Sample Scheduling</b>
-            </div>
+
+            <h3>Shopping List</h3>
+            <form action="index.php" method="post" id="taskform">
+                <input type="hidden" name="id" value="<?php if($editing) { echo $taskData['id']; } ?>"  >
+                <input type="text" name="task" id="task" placeholder="Next item..." value="<?php if($editing) { echo $taskData['task']; } ?>" />
+                <button type="submit" name="submit" id="add"><?php if($editing) { echo "Update"; } else { echo "Add" ; } ?></button>
+            </form>
         </div>
-    </nav> -->
-    <div class="container py-5" id="page-container">
-        <div class="row">
-            <div class="col-md-9">
-                <div id="calendar"></div>
+
+        <div id="tasks">
+            <?php
+                if(!empty($tasks)) {
+                    foreach($tasks as $t) {
+            ?>
+            <div class="task">
+                <span><?php echo $t['task'] ?></span>
+                <a href="index.php?action=edit&id=<?php echo $t['id'] ?>" class="edit button"><i class="fa fa-edit"></i></a>
+                <a onclick="return confirm('Do you want to delete this record?')" href="index.php?action=delete&id=<?php echo $t['id'] ?>" class="delete button"><i class="fa fa-trash-alt"></i></a>
             </div>
-            <div class="col-md-3">
-                <div class="cardt rounded-0 shadow">
-                    <div class="card-header bg-gradient bg-primary text-light">
-                        <h5 class="card-title">Meal Form</h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="container-fluid">
-                            <form action="save_schedule.php" method="post" id="schedule-form">
-                                <input type="hidden" name="id" value="">
-                                <div class="form-group mb-2">
-                                    <label for="title" class="control-label">Title</label>
-                                    <input type="text" class="form-control form-control-sm rounded-0" name="title" id="title" required>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="description" class="control-label">Description</label>
-                                    <textarea rows="3" class="form-control form-control-sm rounded-0" name="description" id="description" required></textarea>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="start_datetime" class="control-label">Start</label>
-                                    <input type="datetime-local" class="form-control form-control-sm rounded-0" name="start_datetime" id="start_datetime" required>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="end_datetime" class="control-label">End</label>
-                                    <input type="datetime-local" class="form-control form-control-sm rounded-0" name="end_datetime" id="end_datetime">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="text-center">
-                            <button class="btn btn-primary btn-sm rounded-0" type="submit" form="schedule-form"><i class="fa fa-save"></i> Save</button>
-                            <button class="btn btn-default border btn-sm rounded-0" type="reset" form="schedule-form"><i class="fa fa-reset"></i> Cancel</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php }} ?>
+        </div>
         </div>
     </div>
-    <!-- Event Details Modal -->
-    <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content rounded-0">
-                <div class="modal-header rounded-0">
-                    <h5 class="modal-title">Meal Details</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body rounded-0">
-                    <div class="container-fluid">
-                        <dl>
-                            <dt class="text-muted">Title</dt>
-                            <dd id="title" class="fw-bold fs-4"></dd>
-                            <dt class="text-muted">Description</dt>
-                            <a id="descriptionLink" href="#" onclick="myFunction()"> <dd id="description" class=""></dd> </a>
-                            <dt class="text-muted">Start</dt>
-                            <dd id="start" class=""></dd>
-                            <dt class="text-muted">End</dt>
-                            <dd id="end" class=""></dd>
-                        </dl>
-                    </div>
-                </div>
-                <div class="modal-footer rounded-0">
-                    <div class="text-end">
-                        <button type="button" class="btn btn-primary btn-sm rounded-0" id="edit" data-id="">Edit</button>
-                        <button type="button" class="btn btn-danger btn-sm rounded-0" id="delete" data-id="">Delete</button>
-                        <button type="button" class="btn btn-secondary btn-sm rounded-0" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Event Details Modal -->
 
-<?php 
-$username = $_COOKIE['username'];
-//$schedules = $conn->query("SELECT * FROM `schedule_list`");
-$schedules = mysqli_query(dbConnection(), "SELECT * FROM `schedule_list` WHERE usersUid = '{$username}'");
-$sched_res = [];
-foreach($schedules->fetch_all(MYSQLI_ASSOC) as $row){
-    $row['sdate'] = date("F d, Y h:i A",strtotime($row['start_datetime']));
-    $row['edate'] = date("F d, Y h:i A",strtotime($row['end_datetime']));
-    $sched_res[$row['id']] = $row;
-}
-?>
-<?php 
-if(isset($conn)) $conn->close();
-?>
+    <br><br><br><button onclick="window.print()">Print this page</button>
+
+
+    <script>
+    // function search(event) {
+    //   event.preventDefault();
+    //   var keywords = document.getElementById("search-box").value;
+    //   var url1 = "https://www.traderjoes.com/home/search?q=" + keywords;
+    //   var url2 = "https://www.vistaprint.com/search?query=" + keywords;
+    //   var iframefr = document.getElementById("search-results-fr");
+    //   var iframeen = document.getElementById("search-results-en");
+    //   iframefr.src = url1;
+    //   iframeen.src = url2;
+    // }
+
+    function shoprite() {
+        var keywords = document.getElementById("search-box").value;
+        var urlShoprite = "https://www.shoprite.com/results?q=" + keywords;
+        window.open(urlShoprite);
+    }
+
+    function traderjoes() {
+        var keywords = document.getElementById("search-box").value;
+        var urlTraderJoes = "https://www.traderjoes.com/home/search?q=" + keywords;
+        window.open(urlTraderJoes);
+    }
+
+    function walmart() {
+        var keywords = document.getElementById("search-box").value;
+        var urlWalmart = "https://www.walmart.com/search?q=" + keywords;
+        window.open(urlWalmart);
+    }
+
+    function wholefoods() {
+        var keywords = document.getElementById("search-box").value;
+        var urlWholeFoods = "https://www.wholefoodsmarket.com/search?text=" + keywords;
+        window.open(urlWholeFoods);
+    }
+  </script>
+
+    <script src="app.js"></script>
 </body>
-<script>
-    var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')                     
-</script>
-<script src="./js/script.js"></script>
-
 </html>
