a1cac22cee8209b4dcb35a2680d6d7be3a34f77f
vic292 <123338639+vic292@users.noreply.github.com>
2023-05-08 00:47:11 -0400

    Missing semicolon

--- phpproject01_LoginSystem-MO/database/sql_folder/users.sql
+++ phpproject01_LoginSystem-MO/database/sql_folder/users.sql
@@ -7,4 +7,4 @@ CREATE TABLE `users` (
     `otp` varchar(6) DEFAULT NULL,
   	`otp_expiration` datetime DEFAULT NULL,
   	`date_created` datetime NOT NULL DEFAULT current_timestamp(),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
